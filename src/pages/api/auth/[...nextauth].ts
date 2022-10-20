b3298cd446f19569f0a0207ee4120c7fade65e1b
matias <matifandy@gmail.com>
2022-10-20 20:31:25 -0300

    fix type error googleprovider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
 
+interface ProcessEnv {
+  [key: string]: string | undefined
+}
 
+const ClientId: string = process.env.GOOGLE_CLIENT_ID || '';
+const GoogleClient: string = process.env.GOOGLE_CLIENT_SECRET || '';
 
 export default NextAuth({
     providers: [
       GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientId: ClientId,
+        clientSecret: GoogleClient,
       })
     ],
     secret: 'IamVeryHandsome',
